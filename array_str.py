790fb3481d837f90e118ed9858c30026826efe28
star-uu-wang <uu_wang@foxmail.com>
2024-04-03 20:56:59 +0800

    Update array_str.py

--- array_str.py
+++ array_str.py
@@ -23,6 +23,34 @@ class Solution:
         nums1[m:] = nums2
         nums1.sort()
 
+# S1 套接后排序
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+        nums1[m:] = nums2
+        nums1.sort()
+
+# 指针 -> 两个非递减数组
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        sorted = []
+        p1, p2 = 0, 0
+        while p1 < m or p2 < n:
+            if p1 == m:
+                sorted.append(nums2[p2])
+                p2 += 1
+            elif p2 == n:
+                sorted.append(nums1[p1])
+                p1 += 1
+            elif nums1[p1] < nums2[p2]:
+                sorted.append(nums1[p1])
+                p1 += 1
+            else:
+                sorted.append(nums2[p2])
+                p2 += 1
+        nums1[:] = sorted
+
+
 # 测试用例
 test_cases = [
     ([[1,2,3,0,0,0], 3, [2,5,6], 3], [1,2,2,3,5,6]),
